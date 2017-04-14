d212195742e6592e20ab4b136daf628e14587701
Sören Köhler <soerenkoehler@users.noreply.github.com>
2017-04-14 20:31:41 +0200

    Tampermonkeyskript "Play it once Sam" um APUN-Remover ergänzt

--- Play it once Sam.user.js
+++ Play it once Sam.user.js
@@ -1,24 +1,39 @@
 // ==UserScript==
 // @name         Play it once Sam
 // @namespace    https://gist.github.com/soerenkoehler
-// @version      0.1
+// @version      1.0
 // @description  Starts playlist entries in single video mode instead of playlist mode.
 // @author       https://gist.github.com/soerenkoehler
-// @match        *://www.youtube.com/playlist*
+// @match        *://www.youtube.com/*
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
 
-    filterVideoLinks();
-    window.addEventListener('spfdone', filterVideoLinks);
+    window.addEventListener('spfdone', doAction);
+    doAction();
 })();
 
+function doAction() {
+    removeAPUN();
+    filterVideoLinks();
+}
+
+function removeAPUN() {
+    var autoplaybar = document.getElementsByClassName('autoplay-bar')[0];
+    if (autoplaybar) {
+        autoplaybar.removeAttribute('class');
+        document.getElementsByClassName('checkbox-on-off')[0].remove();
+    }
+}
+
 function filterVideoLinks() {
-    var links = document.querySelectorAll('a.pl-video-title-link');
-    for (var i = 0; i < links.length; i++ ) {
-        processVideoLink(links[i]);
+    if(window.location.pathname == '/playlist') {
+        var links = document.querySelectorAll('a.pl-video-title-link');
+        for (var i = 0; i < links.length; i++ ) {
+            processVideoLink(links[i]);
+        }
     }
 }
 
