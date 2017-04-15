450449373be97b3d9ac30d61e8c05b46450a65f0
Sören Köhler <soerenkoehler@users.noreply.github.com>
2017-04-15 23:00:49 +0200

    einfaches Skript für Google Maps NCR

---
+++ Google Maps NCR.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         Google Maps NCR
+// @namespace    https://github.com/soerenkoehler
+// @version      1.0
+// @description  Always use maps.google.com instead of local variant.
+// @author       https://github.com/soerenkoehler
+// @match        *://www.google.de/maps*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    window.location = 'https://www.google.com/maps/@51,9.5,6z?hl=de';
+})();
