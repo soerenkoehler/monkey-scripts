9624a0772a50bd46ee5e9446f923b314e664cdb0
Sören Köhler <soerenkoehler@users.noreply.github.com>
2017-04-15 19:23:34 +0200

    Heise Topper: Content-Area bei Heise.de nach oben verschieben

---
+++ Heise Topper.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         Heise Topper
+// @namespace    https://github.com/soerenkoehler
+// @version      1.0
+// @description  Moves up the content area on heise online, when e.g. banners are suppressed.
+// @author       https://github.com/soerenkoehler
+// @match        *://www.heise.de/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    document.querySelectorAll('div#container_content')[0].style = 'top:0px;';
+})();
