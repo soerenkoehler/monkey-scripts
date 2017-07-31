11fa2d4e43220bd325ce06a49ecab3d94c9f2910
Sören Köhler <soerenkoehler@users.noreply.github.com>
2017-07-31 02:17:30 +0200

    Tampermonkeyskript "ZON Onepage"

---
+++ ZON Onepage.user.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         ZON Onepage
+// @namespace    https://github.com/soerenkoehler
+// @version      1.0
+// @description  Always view Zeit Online articles on one page.
+// @author       https://github.com/soerenkoehler
+// @match        *://www.zeit.de/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    var links = document.getElementsByClassName('article-toc__onesie');
+    if( links.length == 1 ) {
+        window.location.href = links[0].href;
+    }
+})();
