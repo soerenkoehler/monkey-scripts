54f64703282b8f615de887b8be023d5da4e976dc
Sören Köhler <soerenkoehler@users.noreply.github.com>
2017-09-10 00:07:00 +0200

    leo.org Topper: Content-Area bei leo.org nach oben verschieben

---
+++ Leo.org Topper.user.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name         Leo.org Topper
+// @namespace    https://github.com/soerenkoehler
+// @version      1.0
+// @description  Moves up the content area on leo.org online, e.g. when banners are suppressed.
+// @author       https://github.com/soerenkoehler
+// @match        *://*.leo.org/*
+// @run-at       document-end
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    document.querySelectorAll('div#mainContent')[0].style = 'top:'
+        + getHeaderBottom('header.l-dict-header', getHeaderBottom('header.l-www-header', 0))
+        + 'px;';
+})();
+
+function getHeaderBottom(selector, orElse) {
+    var headers = document.querySelectorAll(selector);
+    if( headers.length > 0 ) {
+        return headers[0].getBoundingClientRect().bottom + 5;
+    }
+    return orElse;
+}
