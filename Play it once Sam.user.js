8846d3acee0104c9be3f27d17246d06c4367889f
Sören Köhler <soerenkoehler@users.noreply.github.com>
2017-12-24 18:41:11 +0100

    Update 'Play it once Sam' für 'New Youtube'

--- Play it once Sam.user.js
+++ Play it once Sam.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         Play it once Sam
 // @namespace    https://github.com/soerenkoehler
-// @version      1.0
-// @description  Starts playlist entries in single video mode instead of playlist mode.
+// @version      2.0
+// @description  Modifies window location to play playlist entries in single video mode instead of playlist mode.
 // @author       https://github.com/soerenkoehler
 // @match        *://www.youtube.com/*
 // @run-at       document-end
@@ -12,41 +12,37 @@
 (function() {
     'use strict';
 
-    window.addEventListener('spfdone', doAction);
-    doAction();
+    if(window.location.pathname == '/playlist') {
+        window.addEventListener('yt-navigate-start', goToSinglePlayer);
+    }
+    if(window.location.pathname == '/watch') {
+        window.addEventListener('viewport-load', removeAutoplayButton);
+    }
 })();
 
-function doAction() {
-    removeAPUN();
-    filterVideoLinks();
-}
-
-function removeAPUN() {
-    var autoplaybar = document.getElementsByClassName('autoplay-bar')[0];
-    if (autoplaybar) {
-        autoplaybar.removeAttribute('class');
-        document.getElementsByClassName('checkbox-on-off')[0].remove();
+function goToSinglePlayer() {
+    var url = window.location.href;
+    var sep = url.indexOf('?');
+    var newurl = url.substr(0, sep) + '?' + modifyPlaylistURL(url.substr(sep + 1));
+    if(url != newurl) {
+        window.location.replace(newurl);
     }
 }
 
-function filterVideoLinks() {
-    if(window.location.pathname == '/playlist') {
-        // processAllVideoLinks('a.ytd-playlist-video-renderer');
-        // processAllVideoLinks('a.ytd-thumbnail');
-        processAllVideoLinks('a.pl-video-title-link');
-    }
+function modifyPlaylistURL(query) {
+    var params = {};
+    query.split('&').forEach(function(p) {
+        var sep = p.indexOf('=');
+        params[p.substr(0,sep)] = p.substr(sep+1);
+    });
+    return params.index===undefined?query:'v=' + params.v;
 }
 
-function processAllVideoLinks(selector) {
-    var links = document.querySelectorAll(selector);
-    for (var i = 0; i < links.length; i++ ) {
-        processVideoLink(links[i]);
-    }
-}
-
-function processVideoLink(link) {
-    var paramStart = link.href.indexOf('&');
-    if( paramStart >= 0 ) {
-        link.href = link.href.substr(0, paramStart);
+function removeAutoplayButton() {
+    console.info('Test!');
+    var buttons = document.querySelectorAll('paper-toggle-button.ytd-compact-autoplay-renderer');
+    for(var i = 0; i < buttons.length; i++) {
+        buttons[i].checked = false;
+        buttons[i].fire('change');
     }
 }
