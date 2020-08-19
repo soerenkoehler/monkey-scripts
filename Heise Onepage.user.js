4c8ccc8a262e472f0103cabd74019120a1d22db3
Sören Köhler <soerenkoehler@users.noreply.github.com>
2020-08-19 10:42:27 +0200

    fix heise.de onepage: forum urls

--- Heise Onepage.user.js
+++ Heise Onepage.user.js
@@ -5,6 +5,7 @@
 // @description  Always view heise articles on one page.
 // @author       https://github.com/soerenkoehler
 // @match        *://www.heise.de/*
+// @exclude      *://www.heise.de/forum*
 // @exclude      *://www.heise.de/*seite=all*
 // @exclude      *://www.heise.de/*view=zoom*
 // @run-at       document-end
