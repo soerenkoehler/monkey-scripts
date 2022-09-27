e1663bdb67daaf9aec70e320b5e8977c9222fefc
Sören Köhler <soerenkoehler@users.noreply.github.com>
2022-09-28 01:05:47 +0200

    Update Amazon-Skip-Primescreen.user.js

--- Amazon-Skip-Primescreen.user.js
+++ Amazon-Skip-Primescreen.user.js
@@ -5,19 +5,20 @@
 // @description  Skip Amazon landing page.
 // @author       https://github.com/soerenkoehler
 // @match        *://www.amazon.de/gp/buy/prime*
-// @run-at       document-end
+// @match        *://www.amazon.de/gp/cart*
+// @run-at       document-idle
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
 
-    var button = document.querySelector('.prime-nothanks-button')
+    var button = document.querySelector('.prime-nothanks-button');
     if( !button ) {
-        button = document.querySelector('.prime-no-thanks')
+        button = document.querySelector('.prime-no-thanks');
     }
     if( !button ) {
-        button = document.querySelector('.declineCTA')
+        button = document.querySelector('.declineCTA');
     }
     if( button ) {
         button.click();
