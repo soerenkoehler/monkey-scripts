0a673f23a91626f4fb10c7d23df07c6716e83063
Sören Köhler <soerenkoehler@users.noreply.github.com>
2024-06-14 21:11:16 +0200

    Update Amazon-Skip-Primescreen.user.js

--- Amazon-Skip-Primescreen.user.js
+++ Amazon-Skip-Primescreen.user.js
@@ -2,25 +2,56 @@
 // @name         Amazon-Skip-Primescreen
 // @namespace    https://github.com/soerenkoehler
 // @version      1.0
-// @description  Skip Amazon landing page.
+// @description  Skip Amazon Primescreen.
 // @author       https://github.com/soerenkoehler
-// @match        *://www.amazon.de/gp/buy/prime*
 // @match        *://www.amazon.de/gp/cart*
-// @run-at       document-idle
+// @match        *://www.amazon.de/gp/buy*
+// @run-at       document-body
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
 
-    var button = document.querySelector('.prime-nothanks-button');
-    if( !button ) {
-        button = document.querySelector('.prime-no-thanks');
+    console.log("First run: " + window.location);
+
+    function findPrime() {
+        var button;
+        [
+            '.prime-nothanks-button',
+            '.prime-no-thanks',
+            '.prime-no-button button',
+            '.declineCTA'
+        ].forEach(query => {
+            const elem = document.querySelector(query);
+            if( elem ) {
+                const info = JSON.stringify(elem, ["id", "nodeName", "className"]);
+                console.log("Found: " + info);
+                button = elem;
+            }
+        });
+        return button;
     }
-    if( !button ) {
-        button = document.querySelector('.declineCTA');
+
+    function runLoop(clickcondition, endcondition) {
+        console.log("Start loop: " + window.location);
+        const buttonChecker = window.setInterval(function() {
+            const button = findPrime();
+            if( clickcondition(button) ) {
+                const info = JSON.stringify(button, ["id", "nodeName", "className"]);
+                console.log("Click: " + info);
+                button.click();
+            }
+            if( endcondition(button) ) {
+                console.log("End loop: " + window.location);
+                window.clearInterval(buttonChecker);
+            }
+        }, 500);
     }
-    if( button ) {
-        button.click();
+
+    if( window.location.href.includes("prime") ) {
+        runLoop( e => e != null, e => e != null );
+    } else if( window.location.href.includes("spc") ) {
+        runLoop( e => e != null, e => e == null );
     }
 })();
